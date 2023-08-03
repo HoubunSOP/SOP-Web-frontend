b2d431086b6887c2cb814d31e310a2ff2c2c6f13
misaka10843 <misaka10843@outlook.jp>
2023-08-03 20:17:34 +0800

    添加文章列表页面

--- pages/list/post.vue
+++ pages/list/post.vue
@@ -1,7 +1,44 @@
 <template>
   <mainpage>
     <maincolumn>
-
+      <div class="px-[22px] pt-[18px] border-b-[2px] border-gray-200 overflow-hidden bg-white box-border ">
+        <div class="relative mb-0">
+          <h1 class="m-0 flex ">
+            <span class="inline-block text-[#242a36] text-base font-bold tracking-wide">
+              <i class="fa-duotone fa-list" style="--fa-primary-color: #4453c1; --fa-secondary-color: #4453c1;"></i>
+              文章列表
+            </span>
+          </h1>
+        </div>
+      </div>
+      <div class="ContentContainer">
+        <nuxt-link v-for="index in 10" :key="index"
+          class="pt-[26px] pb-[15px] px-6 relative rounded-md flex flex-wrap overflow-hidden transition-all hover:bg-slate-100 hover:scale-[1.02] ease-in-out"
+          :to="'/post/' + index">
+          <p class="aText overflow-hidden h-[3.8rem] mr-5 text-sm font-medium line-clamp-3">
+            「ごらく部」でまったり日常を送る女子中学生4人組が異世界転生！？人気作「ゆるゆり」のスピンオフ作品！{{ testnum }}
+          </p>
+          <div
+            class="justify-self-end ml-auto w-[120px] h-[72px] md:w-[142px] md:h-[88px] rounded-md overflow-hidden relative">
+            <div class="h-full relative">
+              <nuxt-img loading="lazy" class="w-[100%] h-[100%] absolute top-0 left-0 object-cover"
+                src="https://media.comicspace.jp/wp-content/uploads/2022/01/775a9b3f8aa99181e60620106c09bd65.jpeg"
+                alt="post cover" />
+            </div>
+          </div>
+          <div class="absolute bottom-2.5">
+            <span class="text-xs md:text-sm font-medium tracking-wide text-[#808080]">
+              <i class="fa-duotone fa-list-tree"></i>
+              コミスペ！
+            </span>
+            <span class="text-xs md:text-sm font-medium tracking-wide text-[#808080]">
+              <i class="fa-duotone fa-calendar-week"></i>
+              2022.01.11
+            </span>
+          </div>
+        </nuxt-link>
+      </div>
+      <compsPagination :current-page="currentPage" :total-pages="totalPages" @page-changed="onPageChanged" />
     </maincolumn>
     <Sidebar />
   </mainpage>
@@ -9,8 +46,27 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      currentPage: 1,
+      totalPages: 10,
+      testnum: 1,
+      // 其他请求参数
+    };
+  },
+  methods: {
+    onPageChanged (page) {
+      this.currentPage = page;
+      // 更新您的 API 请求参数
+      // 例如：this.fetchData(this.currentPage);
+      this.testnum = this.testnum + 1;
+    },
+  },
 }
 </script>
 
-<style></style>
+<style>
+.aText {
+  width: calc(100% - 162px);
+}
+</style>
