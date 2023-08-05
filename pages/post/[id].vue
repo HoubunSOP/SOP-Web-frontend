379ad02992a4593c80a33f037a9294bd787f6284
misaka10843 <misaka10843@outlook.jp>
2023-08-05 16:15:50 +0800

    添加post css的附属资源

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -21,36 +21,409 @@
           </div>
         </header>
         <div id="post-content">
-          <h2>『転生したらあかりだけスライムだった件』</h2>
-
-          <p>さて。本日は、かの「転スラ」を紹介しよう。<br>
-            転スラといえばみなさんご存じ、例のアレだ。現実の地球から洋風ファンタジー世界へ飛ばされた主人公が人の身を失い、魔物のスライムに生まれ変わるあの作品。そう。</p>
-          <p>『<a href="https://comicspace.jp/title/126990" rel="noopener noreferrer"
-              target="_blank">転生したらあかりだけスライムだった件</a>』だよ！</p>
-          <p>はい、というわけでこちらは一迅社「コミック百合姫」の看板作品『ゆるゆり』からのスピンオフである。</p>
-          <p>
-            主人公・<strong>赤座あかり</strong>、トリックスターにしてトラブルメーカーの<strong>歳納京子</strong>、頼れるツッコッミ系お姉さんポジションの<strong>船見結衣</strong>、その結衣に熱愛を寄せるぶりっ子後輩・<strong>吉川ちなつ</strong>──気ままに遊びとくつろぎを満喫する独自の部活「ごらく部」でまったり日常を送る女子中学生4人組が、舞台を学校から異世界に移す外伝だ。
-          </p>
-          <p>
-            ある日、VRゲーム用ヘッドセットをかぶってRPGに興じていたごらく部の面々。プレイ中の京子と結衣がうっかり屋外へ飛び出し、追いかけたあかりとちなつもろともトラックならぬ三輪車に当たって爆発（！？）。なぜかゲーム内のような別天地へ放り込まれてしまう。
-          </p>
-          <p>
-            空をドラゴンが駆け、地にもモンスターや異種族がひしめき、剣と魔法の戦いが火花散らす未知の世界。そんな状況でもごらく部はすっとぼけたボケとツッコミの連続でわいわいと賑やか。4人とも相変わらずの様子……いや、おかしい。一人おかしいぞ。<span
-              class="highlight"><strong>あかりだけがまんまるぷにぷにしたスライム姿に変貌している</strong></span>ではないか！</p>
-          <p>大人しくいい子なぶん損な目に遭いがちなあかりだが、ついに人間やめさせられるところまで……。</p>
-          <p>とはいえ、お団子ヘアがくっついたままなスライムあかりの造形はなんともキュート。不憫なあつかいのなかでも素朴なかわいらしさをみせて作品の愛嬌あるムードを支えるあたりは一周まわってちゃんと主人公やっているなあ、という観だ。
-          </p>
-          <p>
-            無力で小さなスライムと化したことで構図的には<strong>“ただその場にいて喜怒哀楽を示すだけでも必要じゅうぶんにかわいい”</strong>という、つまりはマスコットキャラクターの機能を担っているわけだが、考えてみればその性質はそもそも元作品におけるあかりの持ち味でもある。だから、キャラを変質させたというよりも本質を抽出した仕立てと見れば感心しどころだろう。
-          </p>
-          <p>お約束の異世界ファンタジー展開に『ゆるゆり』キャラを代入したらどうなるか、興味深いジャンル越境で楽しませる本作。</p>
-          <p>
-            単行本は2021年8月に第1巻が発売され、冒険者ギルドで盗賊や騎士などジョブを獲得したごらく部が同じように現実から飛ばされてきた学校の生徒会メンバー・杉浦綾乃たちと合流してさあこれから、という第6話までが収録された。一方、Web連載は同年9月更新の第7話が最新となって現在に至っている。
-          </p>
-          <p>まだまだ冒険は序盤、描けるであろう展開の余地は大きいのでできれば続きを読みたいところだ。</p>
-          <p>余談だが、『転生したらスライムだった件』を擁する講談社は2016年から一迅社を傘下におさめているので、本作の題名はいわば親会社のヒット作をもじった風情なのは言うまでもない。配信サイトも「<a
-              href="https://seiga.nicovideo.jp/comic/51933" rel="noopener noreferrer"
-              target="_blank">水曜日のシリウス</a>」で転スラ（本家）と並んでるしね。</p>
+          <div id="single_content">
+            <p>
+              バードウォッチング専門誌『
+              <a href="https://birder.jp/" rel="noopener noreferrer" target="_blank">
+                BIRDER
+              </a>
+              』でも特集が組まれるなど、漫画好きだけでなくバードウォッチャーからも注目を集めている『
+              <a href="https://comicspace.jp/title/127620" rel="noopener noreferrer" target="_blank">
+                しあわせ鳥見んぐ
+              </a>
+              』。
+            </p>
+            <p>
+              主人公の芸大生・
+              <strong>
+                宮内すず
+              </strong>
+              は、野鳥観察を趣味にする
+              <strong>
+                時庭翼
+              </strong>
+              に出会い、自分も鳥見（バードウォッチング）を始めてみることに。バードウォッチングの楽しさはもちろん、鳥を観察する上でのマナー、鳥と人間の共生についても誠実に描いた作品です。
+            </p>
+            <p class="text-center">
+              <a class="imageAnchor" href="https://media.comicspace.jp/archives/18988/toriming001">
+                <img permalink="https://media.comicspace.jp/archives/18988/toriming001"
+                  data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming001.jpg" alt="" width="400"
+                  height="568" class="alignnone size-full wp-image-18997"
+                  src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming001.jpg">
+              </a>
+              <br>
+              <span class="caption">
+                単行本1巻書影
+              </span>
+            </p>
+            <p>
+              10月27日の単行本1巻発売を記念して、作者の
+              <strong>
+                わらびもちきなこ
+              </strong>
+              先生と、本作が連載されている『まんがタイムきらら』の担当編集さんにお話を伺いました。
+            </p>
+            <p>
+              （取材・文：
+              <a href="https://www.mashiro-writer.com/" rel="noopener noreferrer" target="_blank">
+                ましろ
+              </a>
+              /編集：
+              <a href="https://media.comicspace.jp/archives/author/kohei" rel="noopener noreferrer" target="_blank">
+                八木光平
+              </a>
+              ）
+            </p>
+            <h2>
+              定期購読していた『BIRDER』とまさかのコラボ
+            </h2>
+            <p>
+              <strong>
+                ──本日はお忙しい中ありがとうございます。この取材を申し込んだ時点では『BIRDER』でもインタビューを受けられていたことを知らなかったので、告知を見たときびっくりしました。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                わらびもちきなこ先生（以下、きなこ）
+              </span>
+              ：私も事前にまったく知らされていなくて、担当さんから突然「コラボ決まったよ！」と言われてびっくりしました（笑）。
+              <strong>
+                <span class="underline">
+                  『BIRDER』は、この漫画を描き始めたころから資料も兼ねて定期購読していた雑誌だった
+                </span>
+              </strong>
+              ので。
+            </p>
+            <p class="text-center">
+              <a class="imageAnchor" href="https://media.comicspace.jp/archives/18988/toriming002">
+                <img permalink="https://media.comicspace.jp/archives/18988/toriming002"
+                  data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming002.jpg" alt="" width="400"
+                  height="564" class="alignnone size-full wp-image-18998"
+                  src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming002.jpg">
+              </a>
+              <br>
+              <span class="caption">
+                『BIRDER』2021年11月号
+              </span>
+            </p>
+            <p>
+              ただ、『BIRDER』のインタビューでは現実のバードウォッチングに関連した話が多かったので、今日はまた違ったお話もできるかなと思っています。
+            </p>
+            <p>
+              <strong>
+                ──インタビューだけでなく、雑誌の表紙も描かれたり、先生も記事を寄稿されたりとかなり大がかりなコラボ企画でしたが、どういった経緯で実現したんでしょうか。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _green">
+                担当編集
+              </span>
+              ：『しあわせ鳥見んぐ』はきららでも今イチオシの作品なので、単行本の発売にあわせてこれまでにない施策がしたいなと考えていました。そんなとき、私も作品資料として読んでいた『BIRDER』に、たまたま『
+              <a href="https://comicspace.jp/title/122732" rel="noopener noreferrer" target="_blank">
+                秋山さんのとりライフ
+              </a>
+              』というバードウォッチング漫画のインタビュー記事が載っていたんです。
+            </p>
+            <p>
+              それで、「ウチにも『しあわせ鳥見んぐ』って漫画があるんですけれども……」と恐る恐るオファーをしてみたら興味を持っていただけて。打ち合わせを重ねるうちにどんどん話が膨らんでいき、最終的に表紙も描かせてもらうことになりました。
+            </p>
+            <p>
+              <strong>
+                ──バードウォッチングの漫画を描こうと思ったのはどうしてですか？
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：前作『
+              <a href="https://comicspace.jp/title/116439" rel="noopener noreferrer" target="_blank">
+                佐藤さんはPJK
+              </a>
+              』の終了が決まって次回作の構想を練っていたのが、ちょうど私がバードウォッチングを始めたころだったんです。
+              <span class="highlight">
+                <strong>
+                  この楽しさを漫画にしたら面白そうだな
+                </strong>
+              </span>
+              と思ったのが最初のきっかけですね。
+            </p>
+            <span class="comicspace-title-parameter" title-id="116439">
+            </span>
+            <p>
+              <strong>
+                ──『佐藤さんはPJK』はハイテンションなギャグ4コマでしたが、今作はストーリー重視で落ち着いた雰囲気だなと感じます。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：実は、前の担当さんからも
+              <strong>
+                「次はギャグじゃない方向にしてみては」
+              </strong>
+              と言われていました。理由はちゃんと聞いてないんですけど、前作の反応とか私の絵柄とかを見て、ギャグ以外の可能性を試そうとしてくださったのかもしれません。
+            </p>
+            <p>
+              <strong>
+                ──前の担当さんからそう言われたときの気持ちはいかがでしたか？
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：『PJK』は私が初めてまともに描いた漫画だったので、
+              <span class="bgline">
+                <strong>
+                  ギャグを封印されたら何を描けばいいんだ……
+                </strong>
+              </span>
+              って正直なところ思いました（笑）。
+            </p>
+            <p>
+              なので、何か主軸になるようなテーマがほしいなと思って。そこで自分がちょうどはまっていたバードウォッチングに白羽の矢が立った感じです。
+            </p>
+            <p class="text-center">
+              <img permalink="" data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming003.jpg" alt=""
+                width="500" height="793" class="alignnone size-full wp-image-18999"
+                src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming003.jpg">
+              <br>
+              <span class="caption">
+                初心者のすずと一緒に、読者もバードウォッチングを学ぶことができる
+              </span>
+            </p>
+            <p>
+              <strong>
+                ──前の担当さんからの助言と、バードウォッチングという題材がうまくマッチしたんですね。バードウォッチングを始めたきっかけは何だったんでしょうか。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：数年前に山形に引っ越しをして、
+              <strong>
+                <span class="underline">
+                  せっかく自然豊かな場所に来たのだからアウトドアな趣味を始めたい
+                </span>
+              </strong>
+              と思いました。最初は山登りに挑戦してみたものの、もともとインドア体質なので長続きしなかったです。
+            </p>
+            <p>
+              そんなとき、市の広報で
+              <strong>
+                探鳥会
+              </strong>
+              というイベントがあることを知り、鳥を見るくらいなら自分にもできるかなと軽い気持ちで参加してみたら、バードウォッチングの魅力に取りつかれてしまいました。
+            </p>
+            <p>
+              <strong>
+                ──『しあわせ鳥見んぐ』の舞台が山形なのも、先生が山形在住だからなんですね。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：はい。山形には
+              <strong>
+                飛島
+              </strong>
+              というバードウォッチャーの聖地と呼ばれている場所もありますし、作中に出てきた宮城の
+              <strong>
+                伊豆沼
+              </strong>
+              にも車でアクセスしやすいから、漫画の舞台としても悪くないかなと。
+            </p>
+            <p class="text-center">
+              <img permalink="" data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming004.jpg" alt=""
+                width="350" height="277" class="alignnone size-full wp-image-19000"
+                src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming004.jpg">
+              <br>
+              <span class="caption">
+                山形市民の憩いの場・霞城公園も登場
+              </span>
+            </p>
+            <h2>
+              「鳥見をする人」と「鳥の姿を切り取る」をかけたタイトル
+            </h2>
+            <p>
+              <strong>
+                ──きらら作品には女子高生のキャラクターが多いと思いますが、本作ですずたちを大学生にしたのはなぜですか？
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：バードウォッチングを取り扱うにしても、鳥を見る──インプットするだけじゃなく、
+              <span class="highlight">
+                <strong>
+                  絵を描いたり写真を撮ったりというアウトプットの要素を入れたかった
+                </strong>
+              </span>
+              んです。そうなると、高校生よりも大学生、それも専門的に学んでいる芸大生のほうが説得力があるかなって。
+            </p>
+            <p class="text-center">
+              <img permalink="" data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming005.jpg" alt=""
+                width="500" height="376" class="alignnone size-full wp-image-19001"
+                src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming005.jpg">
+              <br>
+              <span class="caption">
+                「絵に個性がない」と言われ悩んでいたすずだが、鳥と出会い自信を取り戻す
+              </span>
+            </p>
+            <p>
+              あと、単純に大学生のほうが時間の面でもお金の面でも自由度が高いから、漫画を描くにあたって動かしやすそうというのも理由のひとつです。
+            </p>
+            <p>
+              <strong>
+                ──伊豆沼の回も、顧問の先生に引率されてではなく、自分たちで車を運転して現地まで行っていたのが大学生っぽかったです。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：実は最初、すずは社会人設定だったのですが、他のキャラと予定を合わせやすくするためにみんな大学生になりました。翼より1歳年上で成人しているのはその名残ですね。
+            </p>
+            <p>
+              <strong>
+                ──バードウォッチングを題材にしているのに、すずたちがよく焼き鳥を食べているのが個人的に気になっているのですが……。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：
+              <span class="bgline">
+                <strong>
+                  鳥が好きだからといって鳥を食べないわけじゃないよね
+                </strong>
+              </span>
+              、みたいなギャップが逆に面白いかなあ、と……。すみません、そんなに深い意味はありません（笑）。
+            </p>
+            <p class="text-center">
+              <img permalink="" data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming006.jpg" alt=""
+                width="350" height="488" class="alignnone size-full wp-image-19002"
+                src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming006.jpg">
+              <br>
+              <span class="caption">
+                ご注文は焼き鳥ですか？
+              </span>
+            </p>
+            <p>
+              <strong>
+                ──『しあわせ鳥見んぐ』以外にどんなタイトル案がありましたか？
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：
+              <strong>
+                『鳥見びより』
+              </strong>
+              に、
+              <strong>
+                『アーバンバーダー』
+              </strong>
+              に、
+              <strong>
+                『ハッピーバードデー』
+              </strong>
+              ……。とりあえず、バードウォッチングの漫画だと一目でわかるようなタイトルにしたいと思っていました。
+            </p>
+            <p>
+              今のタイトルも最初は『鳥見んぐ』だけだったんですけど、
+              <strong>
+                「しあわせ」
+              </strong>
+              をつけることで柔らかい雰囲気になったかなと。
+              <strong>
+                <span class="underline">
+                  ストーリーの根本に、童話の「幸せの青い鳥」を意識している
+                </span>
+              </strong>
+              部分がありますし。
+            </p>
+            <p class="text-center">
+              <img permalink="" data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming007.jpg" alt=""
+                width="350" height="940" class="alignnone size-full wp-image-19003"
+                src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming007.jpg">
+              <br>
+              <span class="caption">
+                探していたものはすぐ近くに
+              </span>
+            </p>
+            <p>
+              <strong>
+                ──野暮な質問ですが、「鳥見」と「トリミング」をかけているということでいいんでしょうか。
+              </strong>
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：はい。バードウォッチングの俗称としての「鳥見」に「ing（～をすること）」をつけて「鳥見んぐ」と、
+              <span class="highlight">
+                <strong>
+                  絵や写真で鳥の姿を切り取る
+                </strong>
+              </span>
+              という意味での「トリミング」をかけています。
+            </p>
+            <p>
+              <span class="interviewee _green">
+                担当編集
+              </span>
+              ：きなこ先生、そういう言葉選びがすごく上手ですよね。作中にもよくダジャレが出てきますし。
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：1話に最低1回は鳥に絡めたダジャレを入れないと落ち着かないというか……。ギャグは控えめにと言われてたはずなんですけど、やっぱり『PJK』のノリが抜けていないのかも（笑）。
+            </p>
+            <p>
+              <span class="interviewee _green">
+                担当編集
+              </span>
+              ：第7話の
+              <strong>
+                「クロウさせやがって」
+              </strong>
+              というセリフ。「苦労」とカラスの「クロウ（Crow）」がかかっているのに最初気づかなくて、意味がわかったとき感動しました。
+            </p>
+            <p>
+              <span class="interviewee _red">
+                きなこ
+              </span>
+              ：ネームを出したとき
+              <span class="bgline">
+                <strong>
+                  「なんでここカタカナなんですか？」
+                </strong>
+              </span>
+              って聞かれて、自分で自分のギャグを説明するのが恥ずかしかったです……。
+            </p>
+            <p class="text-center">
+              <img permalink="" data-src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming008.jpg" alt=""
+                width="350" height="1088" class="alignnone size-full wp-image-19004"
+                src="https://media.comicspace.jp/wp-content/uploads/2021/10/toriming008.jpg">
+              <br>
+              <span class="caption">
+                このダジャレがすぐわかった人は玄人
+              </span>
+            </p>
+            <p class="copyright">
+              ©わらびもちきなこ/芳文社
+            </p>
+          </div>
         </div>
       </article>
       <Comment />
